20a64fd04e64807f144058c81435e08af21568b8
Dima Kozuch <dmitrii@seznam.cz>
2019-07-23 13:50:41 +0200

    23072019 1

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,7 @@
 import json
+import datetime
 
-from flask import Flask,  request
+from flask import Flask,  request,  render_template
 
 from classes import VegaGraph, SensLogData
 
@@ -29,6 +30,10 @@ def do_send_graph(endpoint_url, unit_id, timestamp_from, timestamp_to, graph_typ
     vg=VegaGraph(type=graph_type,data=s.get_data())
     vg.generate_visual()
     return vg.export_visual(format)
+    
+@application.route("/")
+def index():
+    return render_template("sensors.html", vegajsobject=(do_send_graph(endpoint_url='http://foodie.lesprojekt.cz:8080/senslogOT/rest/observation', unit_id=1305167549167050, timestamp_from=(datetime.datetime.now()-datetime.timedelta(hours=24)).strftime('%Y-%m-%d %H:%M:%S'), timestamp_to=datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), graph_type='point', format='json')));
 
 if __name__ == "__main__":
     application.run()
